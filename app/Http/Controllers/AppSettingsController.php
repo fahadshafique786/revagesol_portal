87397dbba7e6f1459099c753bc29e227b9f90e8d
fahadshafique786 <60979429+fahadshafique786@users.noreply.github.com>
2023-06-21 20:31:00 +0500

    done

--- app/Http/Controllers/AppSettingsController.php
+++ app/Http/Controllers/AppSettingsController.php
@@ -113,6 +113,7 @@ class AppSettingsController extends Controller
     public function store(Request $request , $app_setting_id = false)
     {
 
+        dd($request->account_id,$request->all());
         /*** Validation BEGIN ****/
         if(!empty($app_setting_id))
         {
